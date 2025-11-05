4299bd3ded6091717d313c320d5bc5b03c5df951
Jack Dickinson <jackdickinson090@hotmail.com>
2025-11-05 21:26:15 +0100

    fix practice mode error

--- components/PraticeWindow.tsx
+++ components/PraticeWindow.tsx
@@ -4,13 +4,20 @@ import { useState } from "react";
 import MapButtons from "./MapButtons";
 
 export default function PracticeWindow() {
+  const [showLocation, setShowLocation] = useState("");
+
+  function checkAnswer(region: string) {
+    setShowLocation(region);
+  }
   return (
     <div className="flex h-[90vh]">
       <div className="bg-blue-200 w-3/4 rounded-md">
-        <MapButtons />
+        <MapButtons checkAnswer={checkAnswer} />
       </div>
       <div className="w-1/4">
-        <article className="border-2 border-neutral-800 rounded-md h-full p-4 ml-4">Hello</article>
+        <article className="border-2 border-neutral-800 rounded-md h-full p-4 ml-4">
+          <div>{showLocation}</div>
+        </article>
       </div>
     </div>
   );
