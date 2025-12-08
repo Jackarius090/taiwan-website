be89dba7ed2de8d96b77e34e03c17553cb148a00
Jack Dickinson <jackdickinson090@hotmail.com>
2025-12-08 18:22:31 +0100

    insert into db working

--- app/actions/scoreboard_data.ts
+++ app/actions/scoreboard_data.ts
@@ -1,6 +1,13 @@
 "use server";
 import { ScoreType } from "@/components/GameWrapper";
+import postgres from "postgres";
 
 export async function uploadScore(score: ScoreType) {
-  console.log(score);
+  const sql = postgres(process.env.DATABASE_URL!, { ssl: "require" });
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS scores (name TEXT, correct int, questions int, incorrectAnswers int)`;
+    await sql`INSERT INTO scores VALUES ("${score.name}, ${score.correct}, ${score.questions}, ${score.incorrectAnswers}")`;
+  } catch {
+    console.log("error");
+  }
 }
