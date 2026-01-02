49d7a379353674ab1caaa6533b3910c144e39c90
Jack Dickinson <jackdickinson090@hotmail.com>
2026-01-02 13:47:42 +0000

    more fireworks progress

--- components/GameWindow.tsx
+++ components/GameWindow.tsx
@@ -199,15 +199,20 @@ export default function GameWindow({ setScoreBoardOpen }: { setScoreBoardOpen: D
             )}
             {state.result && (
               <div className="h-80 relative">
+                <div className="bg-green-500 rounded-md p-2 inset-x-4 top-15 absolute">Correct!</div>
                 <FireworksBackground className="absolute" population={5} fireworkSpeed={20} />
                 <div className="h-1/2 w-full bottom-0 absolute z-10 bg-amber-300"></div>
-                <div className="bg-green-500 rounded-md p-2 inset-x-4 absolute">Correct!</div>
               </div>
             )}
-            {state.showIncorrect && <div className="bg-red-500 rounded-md p-2">Wrong! That&apos;s {state.guess}!</div>}
-            {state.gameRunning && <div className="my-2 p-2">Tries left: {state.tries}</div>}
-            {state.gameRunning && <div className="my-2 p-2">Incorrect: {state.numberIncorrectAnswers}</div>}
-            {state.gameRunning && <div className="my-2 p-2">Points: {state.points}</div>}
+            <div className="relative">
+              {state.showIncorrect && (
+                <div className="bg-red-500 rounded-md p-2">Wrong! That&apos;s {state.guess}!</div>
+              )}
+              {state.gameRunning && <div className="my-2 p-2">Tries left: {state.tries}</div>}
+              {state.gameRunning && <div className="my-2 p-2">Incorrect: {state.numberIncorrectAnswers}</div>}
+              {state.gameRunning && <div className="my-2 p-2">Points: {state.points}</div>}
+            </div>
+
             <div className="grid grid-cols-3 gap-1 mt-6">
               {state.results.map((answer, i) => {
                 return (
